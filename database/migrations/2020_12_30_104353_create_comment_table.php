a9f31d20930a3b08db8a8672d9cf9cd83d715945
Julier <maxime.julier@epitech.eu>
2020-12-30 13:06:26 +0100

    migration-model

---
+++ database/migrations/2020_12_30_104353_create_comment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id();
+            $table->text("text");
+            $table->bigInteger("comment_id");
+            $table->bigInteger("photo_id");
+            $table->bigInteger("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
